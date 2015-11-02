afa9e07f150b6501e33a21e8ac1d4a003f86b0a5
Marianela <mari.ytu@gmail.com>
2015-11-02 01:44:37 -0300

    implementacion de getColumnsNoise

--- R/NoiseDetection.R
+++ R/NoiseDetection.R
@@ -1,3 +1,13 @@
+#' Calculate Diff
+#'
+#' Function to calculate de variation of a dataSet.
+#' 
+#' @param dataSet an object of class data frame with a data set.
+#' @return an object of class data frame with a variation of all data set.
+#' @seealso getColumnsNoise
+#' @examples
+#' iris.x <- iris[,1:4]
+#' diffValues <- calculateDiff(iris.x)
 calculateDiff <- function(dataSet) {
   
   if (missing(dataSet)) {
@@ -19,10 +29,23 @@ calculateDiff <- function(dataSet) {
   return (resp)
 }
 
-getColumnsNoise <- function(dataSet, limit) {
+#' Columns Noise
+#'
+#' Function to calculate de variation of a dataSet.
+#' 
+#' @param data an object of class data frame with a data set of variation.
+#' @param limit an double that represent the umbral for detect columns variation.
+#' @return an object of class data frame all columns that contains noise.
+#' @seealso getColumnsNoise
+#' @examples
+#' iris.x <- iris[,1:4]
+#' diffValues <- calculateDiff(iris.x)
+#' limit = 0.15
+#' columnsNoise <- getColumnsNoise(diffValues, limit)
+getColumnsNoise <- function(data, limit) {
   
-  if (missing(dataSet)) {
-    stop("Need to specify dataSet!")
+  if (missing(data)) {
+    stop("Need to specify data!")
   }
   if (missing(limit)) {
     stop("Need to specify limit!")
@@ -31,9 +54,12 @@ getColumnsNoise <- function(dataSet, limit) {
   #All parameters are OK!
   noise <- data.frame(j=integer(),stringsAsFactors=FALSE)
   
-  for (i in 1:nrow(dataSet)) {
-    for (j in 1:ncol(dataSet)) {
-      if (dataSet[i,j] >= limit) {
+  for (j in 1:ncol(data)) {
+    x <- data[,j]
+    aux <- count(x >= limit)
+    
+    for (i in 1:nrow(aux)) {
+      if (aux[i,1]==TRUE) {
         noise[nrow(noise)+1,1] <- j
       }
     }
